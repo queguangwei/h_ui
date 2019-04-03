f9790dc572b1cd5029b779aaf3fe16974c2b0c9f
luwuer <html6@foxmail.com>
2019-04-03 21:12:47 +0800

    [form]添加 resetValidate 和 resetValidateField 方法

--- src/components/Form/Form.vue
+++ src/components/Form/Form.vue
@@ -1,219 +1,269 @@
 <template>
-    <form :class="classes"><slot></slot></form>
+  <form :class="classes">
+    <slot></slot>
+  </form>
 </template>
 <script>
-import { oneOf,hasClass,cmp} from  '../../util/tools';
+import { oneOf, hasClass, cmp } from '../../util/tools'
 
-const prefixCls = 'h-form';
+const prefixCls = 'h-form'
 
 export default {
   name: 'Form',
   props: {
     model: {
-        type: Object
+      type: Object
     },
-    compareModel:{
-      type:Object
+    compareModel: {
+      type: Object
     },
     rules: {
-        type: Object
+      type: Object
     },
     labelWidth: {
-        type: Number
+      type: Number
     },
     labelPosition: {
-        validator (value) {
-            return oneOf(value, ['left', 'right', 'top']);
-        },
-        default: 'right'
+      validator(value) {
+        return oneOf(value, ['left', 'right', 'top'])
+      },
+      default: 'right'
     },
     inline: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     showMessage: {
-        type: Boolean,
-        default: true
+      type: Boolean,
+      default: true
     },
     cols: {
-        type: [String, Number]
+      type: [String, Number]
     },
-    placement:{
-      validator (value) {
-        return oneOf(value, ['null','top', 'top-start', 'top-end', 'bottom', 'bottom-start', 'bottom-end', 'left', 'left-start', 'left-end', 'right', 'right-start', 'right-end']);
+    placement: {
+      validator(value) {
+        return oneOf(value, [
+          'null',
+          'top',
+          'top-start',
+          'top-end',
+          'bottom',
+          'bottom-start',
+          'bottom-end',
+          'left',
+          'left-start',
+          'left-end',
+          'right',
+          'right-start',
+          'right-end'
+        ])
       },
       default: 'null'
     },
-    errorFocus:{
-      type:Boolean,
-      default:false,
+    errorFocus: {
+      type: Boolean,
+      default: false
     },
     labelWrap: {
       type: Boolean,
       default: null
     },
-    onlyBlurRequire:{
+    onlyBlurRequire: {
       type: Boolean,
       default: false
     }
-
   },
-  data () {
+  data() {
     return {
-        fields: [],
-        validMsgList: [],
-        changeObj:{},
-        isCheck: true,
-    };
+      fields: [],
+      validMsgList: [],
+      changeObj: {},
+      isCheck: true
+    }
   },
   computed: {
-    classes () {
+    classes() {
       return [
-          `${prefixCls}`,
-          `${prefixCls}-label-${this.labelPosition}`,
-          {
-              [`${prefixCls}-inline`]: this.inline,
-              [`${prefixCls}-row`]: this.cols && parseInt(this.cols) <= 12,
-          }
-      ];
+        `${prefixCls}`,
+        `${prefixCls}-label-${this.labelPosition}`,
+        {
+          [`${prefixCls}-inline`]: this.inline,
+          [`${prefixCls}-row`]: this.cols && parseInt(this.cols) <= 12
+        }
+      ]
     }
   },
   methods: {
     resetFields(isResetReadonlyOrDisabled = false) {
-        this.fields.forEach(field => {
-          field.resetField(isResetReadonlyOrDisabled);
-        });
-        this.validMsgList = []
-        this.isCheck = false;
-        setTimeout(()=>{
-          this.isCheck = true;
-        },0)
+      this.fields.forEach(field => {
+        field.resetField(isResetReadonlyOrDisabled)
+      })
+      this.validMsgList = []
+      this.isCheck = false
+      setTimeout(() => {
+        this.isCheck = true
+      }, 0)
     },
     validate(callback) {
       this.validMsgList = []
-      let valid = true;
-      let count = 0;
+      let valid = true
+      let count = 0
       this.fields.forEach(field => {
         if (field.$children && field.$children.length > 0) {
-          for(let fidleChild of field.$children) {
+          for (let fidleChild of field.$children) {
             if (fidleChild.disabled) {
               // ++count;
-              if(++count === this.fields.length){
-                callback(valid);
+              if (++count === this.fields.length) {
+                callback(valid)
               }
-              return ;
+              return
             }
           }
         }
-        field.commonRule();
+        field.commonRule()
         field.validate('', errors => {
           if (errors) {
-            valid = false;
+            valid = false
           }
-          if (typeof callback === 'function' && ++count === this.fields.length) {
-              callback(valid);
+          if (
+            typeof callback === 'function' &&
+            ++count === this.fields.length
+          ) {
+            callback(valid)
           }
-        });
-      });
+        })
+      })
       // if (typeof callback === 'function') {
       //   callback(valid);
       // }
       if (this.errorFocus) {
-        this.$nextTick(()=>{
-          for (var i = 0; i < this.fields.length ; i++) {
-            if(hasClass(this.fields[i].$el,'h-form-item-error')){
-              this.fields[i].$children[0].focus();
-              break;
+        this.$nextTick(() => {
+          for (var i = 0; i < this.fields.length; i++) {
+            if (hasClass(this.fields[i].$el, 'h-form-item-error')) {
+              this.fields[i].$children[0].focus()
+              break
             }
           }
         })
       }
     },
     validateField(prop, cb) {
-        this.validMsgList = []
-        const field = this.fields.filter(field => field.prop === prop)[0];
-        if (!field) { throw new Error('[HUI warn]: must call validateField with valid prop string!'); }
-        field.validate('', cb);
+      this.validMsgList = []
+      const field = this.fields.filter(field => field.prop === prop)[0]
+      if (!field) {
+        throw new Error(
+          '[HUI warn]: must call validateField with valid prop string!'
+        )
+      }
+      field.validate('', cb)
     },
-    setPlacement(){
-      this.$nextTick(()=>{
-        this.fields.forEach((field)=>{
+    /**
+     * @description 重置单个字段校验
+     */
+    resetValidateField(prop, cb = null) {
+      const field = this.fields.filter(field => field.prop === prop)[0]
+      if (!field) {
+        throw new Error(
+          '[HUI warn]: must call resetValidate with valid prop string!'
+        )
+      }
+
+      field.resetValidate(cb)
+    },
+    /**
+     * @description 重置所有字段校验
+     */
+    resetValidate(cb = null) {
+      this.fields.forEach(field => {
+        field.resetValidate(cb)
+      })
+    },
+    setPlacement() {
+      this.$nextTick(() => {
+        this.fields.forEach(field => {
           if (field.$children && field.$children.length > 0) {
-            for(let fidleChild of field.$children) {
+            for (let fidleChild of field.$children) {
               if (fidleChild.placement) {
-                fidleChild.fPlacement = this.placement;
+                fidleChild.fPlacement = this.placement
               }
             }
           }
-        });
-      });
-    },
-    changeStyle(obj){
-      if (!this.compareModel || this.compareModel == {}) return;
-      for (let item in obj) {
-        let status = true;
-        // if (obj[item] == this.compareModel[item] || this.compareModel[item] == undefined) {
-        //   status = false;
-        // }
-        if (cmp(obj[item],this.compareModel[item]) || this.compareModel[item] == undefined) {
-          status = false;
-        }
-        this.changeObj[item]=status;
-      }
-      this.fields.forEach(col=>{
-        col.modeChanged = this.changeObj[col.prop];
-      });
-      
-    },
-    firstNodeFocused(){
-      this.$nextTick(()=>{
-        let index = 0;
-        for(var i=0;i<this.fields.length;i++){
-          if(!this.fields[i].$children[0].disabled&&!this.fields[i].$children[0].readonly){
-            index = i;
-            break;
-          }
-        }
-        this.fields[i].$children[0].focus();
-        if(this.fields[i].$children[0].opened) this.fields[i].$children[0].opened = false;
-        this.$nextTick(()=>{
-          if(this.fields[i].$children[0].visible) this.fields[i].$children[0].visible=false;
         })
       })
     },
+    changeStyle(obj) {
+      if (!this.compareModel || this.compareModel == {}) return
+      for (let item in obj) {
+        let status = true
+        // if (obj[item] == this.compareModel[item] || this.compareModel[item] == undefined) {
+        //   status = false;
+        // }
+        if (
+          cmp(obj[item], this.compareModel[item]) ||
+          this.compareModel[item] == undefined
+        ) {
+          status = false
+        }
+        this.changeObj[item] = status
+      }
+      this.fields.forEach(col => {
+        col.modeChanged = this.changeObj[col.prop]
+      })
+    },
+    firstNodeFocused() {
+      this.$nextTick(() => {
+        // let index = 0
+        for (var i = 0; i < this.fields.length; i++) {
+          if (
+            !this.fields[i].$children[0].disabled &&
+            !this.fields[i].$children[0].readonly
+          ) {
+            // index = i
+            break
+          }
+        }
+        this.fields[i].$children[0].focus()
+        if (this.fields[i].$children[0].opened)
+          this.fields[i].$children[0].opened = false
+        this.$nextTick(() => {
+          if (this.fields[i].$children[0].visible)
+            this.fields[i].$children[0].visible = false
+        })
+      })
+    }
   },
   watch: {
     rules: {
-      deep:true,
-      handler(val){
-        this.validate();
+      deep: true,
+      handler() {
+        this.validate()
       }
     },
-    placement(){
-      if(this.placement=='null') return;
-      this.setPlacement();
+    placement() {
+      if (this.placement == 'null') return
+      this.setPlacement()
     },
-    model:{
-      deep:true,
-      handler(val){
-        this.changeStyle(val);
+    model: {
+      deep: true,
+      handler(val) {
+        this.changeStyle(val)
       }
     }
   },
-  created () {
-    this.$on('on-form-item-add', (field) => {
-      if (field) this.fields.push(field);
-      return false;
-    });
-    this.$on('on-form-item-remove', (field) => {
-      if (field.prop) this.fields.splice(this.fields.indexOf(field), 1);
-      return false;
-    });
+  created() {
+    this.$on('on-form-item-add', field => {
+      if (field) this.fields.push(field)
+      return false
+    })
+    this.$on('on-form-item-remove', field => {
+      if (field.prop) this.fields.splice(this.fields.indexOf(field), 1)
+      return false
+    })
   },
-  mounted(){
-    if(this.placement!='null'){
-      this.setPlacement();
-    } 
+  mounted() {
+    if (this.placement != 'null') {
+      this.setPlacement()
+    }
   }
-};
+}
 </script>
