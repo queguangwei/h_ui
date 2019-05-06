c7269437f7cca9d79fd5e7c73d8855ffa3171964
ChaochengRen <lensnikor@gmail.com>
2019-05-06 16:16:24 +0800

    bugfix

--- src/components/Table/mixin.js
+++ src/components/Table/mixin.js
@@ -29,10 +29,6 @@ export default {
             } else if (this.columnsWidth[column._index]) {
                 width = this.columnsWidth[column._index].width;
             }
-            // when browser has scrollBar,set a width to resolve scroll position bug
-            if (this.columns.length === index + 1 && top && this.$parent.bodyHeight !== 0 && this.$parent.data.length>0) {
-                width += this.$parent.scrollBarWidth;
-            }
             // when fixed type,reset first right fixed column's width
             if (this.fixed === 'right') {
                 // const firstFixedIndex = this.columns.findIndex((col) => col.fixed === 'right');
