b773bddd1bf3c2f47519e2f028216e190e7477a7
luwuer <html6@foxmail.com>
2019-06-05 19:43:00 +0800

    [select] 移除注释

--- src/components/Select/Select.vue
+++ src/components/Select/Select.vue
@@ -1179,14 +1179,10 @@
       },
       setPlacement(top = 0){
         if(this.autoPlacement){
-            let obj = this.$refs.select;
             let clientHeight = document.documentElement.clientHeight;
-            let rect = obj.getBoundingClientRect()
-            // let scrollTop = document.body.scrollTop || document.documentElement.scrollTop
-            // let curbottom = clientHeight + scrollTop - obj.offsetTop  - obj.clientHeight - top;
+            let rect = this.$refs.select.getBoundingClientRect()
             let bottomNum = this.isCheckall ? 250 : 210;
             if (clientHeight - rect.top - rect.height < bottomNum){
-            // if(curbottom < bottomNum){
               this.fPlacement = 'top';
             } else {
               this.fPlacement = 'bottom';
