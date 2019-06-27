a4194f2dbf645bdc84c984bf0cfc9ebcb768643b
hundsun <hs_ui_jresUI@163.com>
2019-06-27 19:37:27 +0800

    推拽错位问题

--- example/views/hello.vue
+++ example/views/hello.vue
@@ -1,5 +1,148 @@
 <template>
 <div>
+    <Button @click="change"></Button>
+    <h-table width="850" height="300" border :columns="columns2" :data="data3" notSetWidth></h-table>
+</div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                columns2: [
+                    {
+                        title: '姓名1111111111',
+                        key: 'name',
+                        // width: 100,
+                    },
+                    {
+                        title: '年龄111111111111111111111',
+                        key: 'age',
+                        // width: 100
+                    },
+                    {
+                        title: '省份111111111111111111',
+                        key: 'province',
+                        // width: 100
+                    },
+                    {
+                        title: '市区11111111111111',
+                        key: 'city',
+                        // width: 100
+                    },
+                    {
+                        title: '地址1111111111111111111111',
+                        key: 'address',
+                        // width: 200
+                    },
+                    {
+                        title: '邮编111111111111111111111111',
+                        key: 'zip',
+                        // width: 100
+                    },
+                    // {
+                    //     title: '操作',
+                    //     key: 'action',
+                    //     width: 120,
+                    //     render: (h, params) => {
+                    //         return h('div', [
+                    //             h('Button', {
+                    //                 props: {
+                    //                     type: 'text',
+                    //                     size: 'small'
+                    //                 }
+                    //             }, '查看'),
+                    //             h('Button', {
+                    //                 props: {
+                    //                     type: 'text',
+                    //                     size: 'small'
+                    //                 }
+                    //             }, '编辑')
+                    //         ]);
+                    //     }
+                    // }
+                ],
+                // data3:[],
+                data3: [
+                    {
+                        name: '王小明',
+                        age: 18,
+                        address: '北京市朝阳区芍药居',
+                        province: '北京市',
+                        city: '朝阳区',
+                        zip: 100000
+                    },
+                    {
+                        name: '张小刚',
+                        age: 25,
+                        address: '北京市海淀区西二旗',
+                        province: '北京市',
+                        city: '海淀区',
+                        zip: 100000
+                    },
+                    {
+                        name: '李小红',
+                        age: 30,
+                        address: '上海市浦东新区世纪大道',
+                        province: '上海市',
+                        city: '浦东新区',
+                        zip: 100000
+                    },
+                    // {
+                    //     name: '周小伟',
+                    //     age: 26,
+                    //     address: '深圳市南山区深南大道',
+                    //     province: '广东',
+                    //     city: '南山区',
+                    //     zip: 100000
+                    // },
+                    //                     {
+                    //     name: '王小明',
+                    //     age: 18,
+                    //     address: '北京市朝阳区芍药居',
+                    //     province: '北京市',
+                    //     city: '朝阳区',
+                    //     zip: 100000
+                    // },
+                    // {
+                    //     name: '张小刚',
+                    //     age: 25,
+                    //     address: '北京市海淀区西二旗',
+                    //     province: '北京市',
+                    //     city: '海淀区',
+                    //     zip: 100000
+                    // },
+                    // {
+                    //     name: '李小红',
+                    //     age: 30,
+                    //     address: '上海市浦东新区世纪大道',
+                    //     province: '上海市',
+                    //     city: '浦东新区',
+                    //     zip: 100000
+                    // },
+                    // {
+                    //     name: '周小伟',
+                    //     age: 26,
+                    //     address: '深圳市南山区深南大道',
+                    //     province: '广东',
+                    //     city: '南山区',
+                    //     zip: 100000
+                    // }
+                ]
+            }
+        },
+        methods:{
+            change(){
+                this.columns2.push({
+                    title:"111111111111111111111111111111111",
+                    key:'city'
+                })
+            }
+        }
+    }
+</script>
+
+<!--<template>
+<div>
     <h-table height="300" border :columns="columns2" :data="data4" fixedAutoHeight></h-table>
     <button @click="ttt">gengxinshuju</button>
 </div>
@@ -163,6 +306,7 @@
 </script>
 
 </script>
+-->
 <!-- <template>
 <div>
     <h-form ref="formValidate" :model="formValidate" cols="2" :label-width="80">
