489925a1577e19cbda8d87e1c0464018184d8c1e
hundsun <hs_ui_jresUI@163.com>
2019-08-09 15:10:49 +0800

    single-select query-bug

--- example/views/hello.vue
+++ example/views/hello.vue
@@ -2,7 +2,7 @@
 <div>
     <Button @on-click="changeData">改变数值</Button>
     <h-form ref="formValidate" :model="formValidate" cols="2" :label-width="80">
-        <h-form-item label="input" prop="name">
+        <!-- <h-form-item label="input" prop="name">
             <h-input v-model="formValidate.name" placeholder="请输入姓名" class="curItemClass" ></h-input>
         </h-form-item>
         <h-form-item label="typefield" prop="mail">
@@ -14,11 +14,11 @@
                 <h-option value="shanghai">上海市</h-option>
                 <h-option value="shenzhen">深圳市</h-option>
             </h-select>
-        </h-form-item>
+        </h-form-item> -->
         {{formValidate.city}}
-        <h-form-item label="singleSelect" prop="city">
+        <h-form-item label="singleSelect" prop="city" required>
             <h-single-select v-model="formValidate.city" placeholder="请选择所在地" class="curItemClass" transfer>
-                <h-select-block :data="bigData"></h-select-block>
+                <h-select-block :data="bigData" :showCol="showCol"></h-select-block>
             </h-single-select>
         </h-form-item>
         <h-form-item prop="date" label="data">
@@ -44,6 +44,7 @@
         <h-form-item>
             <h-button type="primary" canFocus @click="handleSubmit('formValidate')" class="curItemClass" >提交</h-button>
             <h-button type="ghost"  canFocus  @click="handleReset('formValidate')" style="margin-left: 8px" class="curItemClass" >重置</h-button>
+            <h-button @click="focusFirst">焦点定位到第一项</h-button>
         </h-form-item>
     </h-form>
 </div>
@@ -53,6 +54,7 @@ import { enterHandler1 } from "../../src/util/tools.js";
 export default {
   data() {
     return {
+      showCol:['label1'],
       formValidate: {
         name: "",
         mail: "",
@@ -65,17 +67,17 @@ export default {
         desc: ""
       },
       bigData: [
-        { value: "value1", label: "label1" },
-        { value: "value2", label: "label2" },
-        { value: "value3", label: "label3" },
-        { value: "value4", label: "label4" },
-        { value: "value5", label: "label5" },
-        { value: "value6", label: "label6" },
-        { value: "value7", label: "label7" },
-        { value: "value8", label: "label8" },
-        { value: "value9", label: "label9" },
-        { value: "value10", label: "label10" },
-        { value: "value11", label: "label11" },
+        { value: "value1", label: "label1",label1: "多列1"},
+        { value: "value2", label: "label2",label1: "多列2" },
+        { value: "value3", label: "label3",label1: "多列3" },
+        { value: "value4", label: "label4",label1: "多列4" },
+        { value: "value5", label: "label5",label1: "多列5" },
+        { value: "value6", label: "label6",label1: "多列6" },
+        { value: "value7", label: "label7",label1: "多列7" },
+        { value: "value8", label: "label8",label1: "多列8" },
+        { value: "value9", label: "label9",label1: "多列9" },
+        { value: "value10", label: "label10",label1: "多列10" },
+        { value: "value11", label: "label11",label1: "多列11" },
       ]
     }
   },
@@ -86,8 +88,14 @@ export default {
     });
   },
   methods:{
+    handleSubmit(){
+        this.formValidate.city="value1"
+    },
     changeData(){
         this.bigData = this.bigData.slice(0,2)
+    },
+    focusFirst(){
+        this.$refs.formValidate.firstNodeFocused()
     }
   }
 };
