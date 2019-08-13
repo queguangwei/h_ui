b0c3bac62d05a2f39011e86557cff228c62db221
HS\liyb28050 <liyb28050@hundsun.com>
2019-08-13 15:15:08 +0800

    typefield小数位超出后不允许输入

--- example/views/Typefield.vue
+++ example/views/Typefield.vue
@@ -4,17 +4,12 @@
     <h1>金额</h1>
     <h2>基本用法</h2>
     <h1>algin设置左中右</h1>
-    <p>未开启四舍五入 整数位3位，小数位3为</p>
-    <h-typefield  v-model="valuetest1" integerNum="3" suffixNum="3" type="money" placeholder="金额" bigTips setNull></h-typefield>
-    <p>开启四舍五入</p>
-    <h-typefield  v-model="valuetest2" integerNum="2" suffixNum="2" type="money" placeholder="金额" bigTips isround></h-typefield>
-
-    <!-- {{value11}}
-      <h-typefield v-model="value11" hoverTips immeDivided focusAllSelect integerNum="17" suffixNum="2" type="money" bigTips placeholder="金额"  @on-blur="s" style="width:300px" algin="left" ref="test"></h-typefield>
+      {{value11}}
+      <h-typefield v-model="value11" hoverTips immeDivided focusAllSelect integerNum="17" suffixNum="2" type="money" bigTips placeholder="金额"  @on-blur="s" style="width:300px" algin="left"></h-typefield>
+      {{value12}}
+      <h-typefield v-model="value12" bigTips focusAllSelect integerNum="13" suffixNum="2" type="money" :max="10" :min="0" :step="0.1" algin="center" style="width:300px" ref="test"></h-typefield>
       <Button @on-click="testClick(true)">获取焦点</Button>
       <Button @on-click="testClick(false)">失去焦点</Button>
-      {{value12}}
-      <h-typefield v-model="value12" bigTips focusAllSelect integerNum="13" suffixNum="2" type="money"  @on-blur="s" style="width:300px" algin="center"></h-typefield>
       {{value13}}
       <h-typefield v-model="value13" focusAllSelect integerNum="7" suffixNum="1" type="money" placeholder="金额" bigTips @on-blur="s" style="width:300px" algin="right"></h-typefield>
     <h1>divided 和 isround设置四舍五入 整数位7位，小数位3为</h1>
@@ -66,59 +61,58 @@
     <h-typefield v-model="cardNo" type="cardNo" maxlength="11" :cardFormat="false" placeholder="卡号" bigTips></h-typefield>
     <br>
     <h1>事件</h1>
-    <h-typefield placeholder="焦点事件" @on-blur="bChange" @on-focus="fChange" @on-keyup="kChange"></h-typefield>-->
+    <h-typefield placeholder="焦点事件" @on-blur="bChange" @on-focus="fChange" @on-keyup="kChange"></h-typefield>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: "inputs",
+  name: 'inputs',
   data() {
     return {
-      value: "",
-      value10: "",
-      value0: "1234",
-      value00: "1234",
-      value000: "1234",
-      value1: "1234",
-      value2: "1234",
-      value3: "1234",
-      value4: "1234",
-      msg: "this is input component",
-      select1: "",
-      select2: "",
-      input1: "px",
-      value11: "1111111111111111111",
-      value12: "",
-      value13: "",
-      cardNo: "",
-      valuetest1:0,
-      valuetest2:0
-    };
+      value:'',
+      value10:'',
+      value0:'1234',
+      value00:'1234',
+      value000:'1234',
+      value1:'1234',
+      value2:'1234',
+      value3:'1234',
+      value4:'1234',
+      msg:'this is input component',
+      select1:'',
+      select2:'',
+      input1: 'px',
+      value11: '1111111111111111111',
+      value12: '',
+      value13: '',
+      cardNo:'',
+    }
   },
   methods: {
-    testClick(val) {
+    testClick(val){
       if (val) {
         this.$refs.test.focus();
-      } else {
+      }else{
         this.$refs.test.blur();
       }
     },
-    getValue() {
-      console.log(this.value);
+    getValue(){
+      console.log(this.value)
     },
-    bChange() {
-      console.log("输入框已失去焦点");
+    bChange(){
+      console.log('输入框已失去焦点');
     },
-    fChange() {
-      console.log("输入框已获得焦点");
+    fChange(){
+      console.log('输入框已获得焦点');
     },
-    kChange(val) {
-      console.log("按钮事件已触发" + val);
+    kChange(val){
+      console.log('按钮事件已触发'+val);
     },
-    s(e) {
+    s(e){
       console.log(e);
     }
   }
-};
+}
 </script>
