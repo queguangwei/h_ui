c5336a3644d5e1d9353116f0aae84c1fb6223739
nibiru613 <nibiru613@163.com>
2019-08-13 15:54:04 +0800

    datepicker add showToday

--- src/components/DatePicker/base/confirm.vue
+++ src/components/DatePicker/base/confirm.vue
@@ -1,12 +1,13 @@
 <template>
-  <div :class="[prefixCls + '-confirm']">
+  <div :class="[prefixCls + '-confirm']" :style="styles">
     <span :class="timeClasses" v-if="showTime" @click="handleToggleTime">
         <template v-if="isTime">{{ t('i.datepicker.selectDate') }}</template>
         <template v-else>{{ t('i.datepicker.selectTime') }}</template>
     </span>
+    <i-button size="small" type="text"  v-if="showToday" @click.native="handleShowToday">{{ t('i.calendar.today') }}</i-button>
     <i-button size="small" type="text"  v-if="showLong" @click.native="handleLongTime">{{ t('i.datepicker.long') }}</i-button>
-    <i-button size="small" type="text" @click.native="handleClear">{{ t('i.datepicker.clear') }}</i-button>
-    <i-button size="small" type="primary" @click.native="handleSuccess">{{ t('i.datepicker.ok') }}</i-button>
+    <i-button size="small" type="text"  v-if="!showToday"     @click.native="handleClear">{{ t('i.datepicker.clear') }}</i-button>
+    <i-button size="small" type="primary"  v-if="!showToday"    @click.native="handleSuccess">{{ t('i.datepicker.ok') }}</i-button>
   </div>
 </template>
 <script>
@@ -22,6 +23,7 @@
           showTime: false,
           isTime: false,
           showLong:false,
+          showToday:false,
           timeDisabled: false
       },
       data () {
@@ -34,6 +36,13 @@
               return {
                   [`${prefixCls}-confirm-time-disabled`]: this.timeDisabled
               };
+          },
+          styles(){
+              let style={}
+              if(this.showToday){
+                  style.textAlign='center'
+              }
+              return style
           }
       },
       methods: {
@@ -49,6 +58,9 @@
           },
           handleLongTime(){
               this.$emit('on-pick-long');
+          },
+          handleShowToday(){
+              this.$emit('on-pick-today');
           }
       }
   };
