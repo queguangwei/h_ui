c6efd86254026a360f4f430f33a8ac8d3139ff32
queguangwei@126.com <sl123456>
2019-08-14 17:30:37 +0800

    'updateselect'

--- src/components/Typefield/Typefield.vue
+++ src/components/Typefield/Typefield.vue
@@ -12,7 +12,6 @@
       :maxlength="maxlength"
       :tabindex="tabindex"
       @blur="blurValue"
-      @keydown.stop="keyDown"
       @input="valChange"
       @change="valChange"
       @focus="focusValue($event)"
@@ -50,6 +49,7 @@ import Locale from "../../mixins/locale";
 import Drop from "../Select/Dropdown.vue";
 import TransferDom from "../../directives/transfer-dom";
 const prefixCls = "h-typefield";
+import { on, off } from '../../util/dom';
 
 function addNum(num1, num2) {
   let sq1, sq2, m;
@@ -268,6 +268,7 @@ export default {
     if (this.value != null || this.value != undefined) {
       this.initValue(String(this.value));
     }
+    on(document,'keydown', this.keyDown);
   },
   methods: {
     keyDown(e) {
@@ -704,6 +705,7 @@ export default {
     if (this.transfer) {
       this.$refs.drop.destroy();
     }
+    off(document,'keydown',this.keyDown)
   }
 };
 </script>
