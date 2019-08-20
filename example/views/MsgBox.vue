7ed1d227f081449d65990da447bcdd13583840e8
HS\liyb28050 <liyb28050@hundsun.com>
2019-08-20 16:07:44 +0800

    修改inputnumber

--- example/views/MsgBox.vue
+++ example/views/MsgBox.vue
@@ -4,14 +4,14 @@
 
     <h-button type="primary"
               @click="modal1 = true">显示对话框</h-button>
-    <!-- <h-msg-box v-model="modal1"
+    <h-msg-box v-model="modal1"
                title="普通的Modal对话框标题"
                :height="2500"
                :scrollable="true"
                :top="0"
                isOriginal
-               class-name="vertical-center-modal"> -->
-    <h-msg-box v-model="modal1">
+               class-name="vertical-center-modal">
+    <!-- <h-msg-box v-model="modal1"> -->
       <p>对话框内容</p>
     </h-msg-box>
 
