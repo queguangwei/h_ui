19af388056da44e37bdca0f627fd0c6de29c5392
queguangwei@126.com <sl123456>
2019-08-28 20:17:41 +0800

    'editGird'

--- example/views/EditGird.vue
+++ example/views/EditGird.vue
@@ -2,10 +2,10 @@
   <div>
     <h2>基础</h2>
     <h3>单选</h3>
-    <Button @click="addDate">增加数据</Button><Button @click="getChangeData">获取改变后的数据</Button>
-    <Button @click="delDate">删除数据</Button>
-    <!-- <Button @click="changeMsg">显示</Button> -->
-    <!-- <h-msg-box v-model = "showMsgBox" width="1000"> -->
+    <Button @click="addDate">增加数据</Button>
+    <Button @click="getChangeData">获取改变后的数据</Button>
+    <Button @click="readOnly">只读</Button>
+    <Button @click="write">可写</Button>
       <h-edit-gird ref="table" border height="500"
         :showEditInput=true
         :columns="columns1"
@@ -29,24 +29,19 @@
         @on-editdate-change="selectchange">
         <p slot='loading'>我是自定义loading</p>
       </h-edit-gird>
-    <!-- </h-msg-box> -->
     <Button @click="getData">获取数据</Button>
     <Button @click="setLoad">切换loading</Button>
-    <!--<p>小</p>-->
-    <!--<h-edit-gird :columns="columns1" :data="data1" @on-current-change="click1" @on-row-click="click1" ref="editGird" width="800" no-data-text="你好呀" :loading="loading"></h-edit-gird>-->
+    <p>小</p>
+    <h-edit-gird :columns="columns1" :data="data1" @on-current-change="click1" @on-row-click="click1" ref="editGird" width="800" no-data-text="你好呀" :loading="loading"></h-edit-gird>
     <!--<p>中</p>-->
     <!--<h-edit-gird :columns="columns1" :data="data1" size="large" :disabled-hover="true" :highlight-row="true" @on-current-change="click1" @on-select-cancel="click1" ref="editGird" stripe :loading="loading"></h-edit-gird>-->
     <!--<p>大</p>-->
-
-<!--     <Button @click="getDate">获取数据</Button>
-    <Button @click = "addDate">新增一行</Button> -->
-
     <h3>多选</h3>
-    <!--<h-edit-gird :columns="columns4" :data="data1" :showEditInput="true" @on-select-all="allSelect" @on-select="select" :rowSelect="true" @on-select-cancel="select" height="200"></h-edit-gird>-->
+    <h-edit-gird :columns="columns4" :data="data1" :showEditInput="true" @on-select-all="allSelect" @on-select="select" :rowSelect="true" @on-select-cancel="select" height="200"></h-edit-gird>
     <!--<h-edit-gird :columns="columns4" :data="data1" @on-select-all="allSelect" @on-selection-change="selsetChange" height="200" :loading="loading"></h-edit-gird>-->
     <h3>直接显示编辑框</h3>
     <Button @click="getChangeData">获取改变后的数据</Button>
-    <!--<h-edit-gird :columns="columns1" ref="table1" :data="data1" size="small" :disabled-hover="true" :highlight-row="true" @on-current-change="click1" :showEditInput="true" height="500" :loading="loading"></h-edit-gird>-->
+    <h-edit-gird :columns="columns1" ref="table1" :data="data1" size="small" :disabled-hover="true" :highlight-row="true" @on-current-change="click1" :showEditInput="true" height="500" :loading="loading"></h-edit-gird>
     <Button @click="setLoad">切换loading</Button>
     <Button @click="addDate">添加一行</Button>
     <p>动态显示隐藏输入框</p>
@@ -60,8 +55,7 @@
           </span>
         </h-col>
       </h-row>
-
-      <!--<h-edit-gird :columns="columnsA" :data="datainput" size="small" :showEditInput="showEditInput" stripe></h-edit-gird>-->
+      <h-edit-gird :columns="columnsA" :data="datainput" size="small" :showEditInput="showEditInput" stripe></h-edit-gird>
   </div>
 </template>
 
@@ -108,28 +102,28 @@ var tData= [
     timing:'16:00:00.00',
     tree:'leaf1'
   },
-//  {
-//    name: '周小伟',
-//    age: 26,
-//    address: '深圳市南山区深南大道',
-//    money: '150.00',
-//    cardId: '6223 5678 1234 5678',
-//    city: '北京',
-//    dating:'2018-03-07',
-//    timing:'16:00:00.00',
-//    tree:'leaf1'
-//  },
-//  {
-//    name: '周小伟',
-//    age: 26,
-//    address: '深圳市南山区深南大道',
-//    money: '150.00',
-//    cardId: '6223 5678 1234 5678',
-//    city: '北京',
-//    dating:'2018-03-07',
-//    timing:'16:00:00.00',
-//    tree:'leaf1'
-//  }
+  {
+    name: '周小伟',
+    age: 26,
+    address: '深圳市南山区深南大道',
+    money: '150.00',
+    cardId: '6223 5678 1234 5678',
+    city: '北京',
+    dating:'2018-03-07',
+    timing:'16:00:00.00',
+    tree:'leaf1'
+  },
+  {
+    name: '周小伟',
+    age: 26,
+    address: '深圳市南山区深南大道',
+    money: '150.00',
+    cardId: '6223 5678 1234 5678',
+    city: '北京',
+    dating:'2018-03-07',
+    timing:'16:00:00.00',
+    tree:'leaf1'
+  }
 ];
 var columns=[
         // {
@@ -290,7 +284,6 @@ export default {
   data() {
     return {
       loading:false,
-      showMsgBox:false,
       showEditInput:true,
       selectList: [],
       columns1: [
@@ -514,13 +507,16 @@ export default {
       }
   },
   methods: {
+    readOnly() {
+      this.columns1[8].type=''
+    },
+    write() {
+      this.columns1[8].type = 'date'
+    },
     sortChange(i,j,k){
       console.log(i);
       console.log(j);
       console.log(k);
-    },
-    changeMsg(){
-      this.showMsgBox = !this.showMsgBox;
     },
     expand(e,status){
       console.log(e);
@@ -600,9 +596,9 @@ export default {
       }
     },
     selectchange(val,i,j){
-      console.log(val);
-      console.log(i);
-      console.log(j);
+//      console.log(val);
+//      console.log(i);
+//      console.log(j);
     },
     getChangeData(){
       console.log(this.$refs.table.getChangeData())
