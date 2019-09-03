5e3a8a957ea18d0638e599e887d1e2ff1619a33a
kawhi <kawhi_site@163.com>
2019-09-03 15:58:14 +0800

    Add example SimpleMultiSelect.vue

--- example/main.js
+++ example/main.js
@@ -1,9 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router/index'
-import Venus from '../src/index.js'
+import Vue from "vue";
+import App from "./App";
+import router from "./router/index";
+import Venus from "../src/index.js";
+/* h_ui 发布包 */
+// import Venus from 'h_ui'
 // import '../src/style/icon/iconfont.js'
 // import Venus from '../dist/h_ui.js'
 // import Venus from '../dist/h_ui.min.js'
@@ -12,14 +14,16 @@ import Venus from '../src/index.js'
 // import Button from '../dist/lib/Button.js'
 // import hello_npm from 'venus_hello_npm'
 // import HFileImport from '../src/components/FileImport'
-Vue.use(Venus)
+Vue.use(Venus);
 // Vue.component('h-file-import', HFileImport)
 // Vue.component('h-upload',Upload)
 // Vue.component('h-button',Button)
 
+// window.isO45 = true;
+
 new Vue({
-  el: '#app',
-  name: 'Venus',
+  el: "#app",
+  name: "Venus",
   router,
   render: h => h(App)
-})
+});
