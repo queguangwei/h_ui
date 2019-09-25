1c3a4062e1770508a91b0f7a4209f419ba649b0f
kawhi <kawhi_site@163.com>
2019-09-25 20:21:47 +0800

    fix some problem

--- src/util/index.js
+++ src/util/index.js
@@ -5,6 +5,7 @@ import isArrayLike from "./isArrayLike";
 import isArrayLikeObject from "./isArrayLikeObject";
 import isKeyMatch from "./isKeyMatch";
 import isObject from "./isObject";
+import noop from "./noop";
 import Popper from "./popper.min";
 
 /**
@@ -12,5 +13,5 @@ import Popper from "./popper.min";
  * @example import _ from "@/util"; _.isKeyMatch(event, "Enter");
  * @author yangyc21550@hundsun.com
  */
-export default { deepCloneAs, debounce, throttle, isArrayLike, isArrayLikeObject, isKeyMatch, isObject };
+export default { deepCloneAs, debounce, throttle, isArrayLike, isArrayLikeObject, isKeyMatch, isObject, noop };
 export { Popper };
