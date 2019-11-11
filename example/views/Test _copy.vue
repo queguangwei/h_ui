45c296eeb2f2e9875efc6d39f9b29a1d54ac5c9d
six.jiang <1056857089@qq.com>
2019-11-11 10:30:11 +0800

    table console.log 去除

--- example/views/Test _copy.vue
+++ example/views/Test _copy.vue
@@ -1,173 +1,98 @@
+
+
 <template>
 <div>
-  
-    <!-- <h-row>
-      <h2>基本用法</h2>
-      <p>设置属性 type 为 date 或 daterange 分别显示选择单日和选择范围类型。<br>
-        设置属性 placement 可以更改选择器出现的方向</p>
-      <h-col span="12">
-      </h-col>
-      <h-col span="12">
-        {{model2}}
-        <h-date-picker v-model="model2" type="daterange" format="yyyy-MM-dd HH:mm"  @on-change="handleChange1"  placement="bottom-end" placeholder="选择日期" valueTypeArr> </h-date-picker>
-       <br><br><br>
-       {{value1}}
-        <h-date-picker :value="value1" format="yyyy年MM月dd日" type="date" placeholder="选择日期" style="width: 200px"></h-date-picker>
-      </h-col>
-    </h-row> -->
-      <h-table :columns="columns" :data="data1" headAlgin="center" border :highlight-row="true" @on-current-change="click1" :loading="loading" bodyAlgin="left">
-        <span slot="loading">我是自定义加载！！！</span>
-      </h-table>
-    
+    <h-form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80">
+        <h-form-item label="姓名" prop="name">
+            <h-input v-model="formValidate.name" placeholder="请输入姓名"></h-input>
+        </h-form-item>
+        <h-form-item label="选择日期">
+            <h-row>
+                <h-col span="11">
+                    <h-form-item prop="date">
+                        <h-date-picker type="date" placeholder="选择日期" v-model="formValidate.date"></h-date-picker>
+                    </h-form-item>
+                </h-col>
+                <h-col span="2" style="text-align: center">-</h-col>
+                <h-col span="11">
+                    <h-form-item prop="time">
+                        <h-time-picker type="time" placeholder="选择时间" v-model="formValidate.time"></h-time-picker>
+                    </h-form-item>
+                </h-col>
+                  <h-col span="11">
+                    <h-form-item prop="timerange">
+                        <h-time-picker type="timerange" placeholder="选择时间" v-model="formValidate.timerange"></h-time-picker>
+                    </h-form-item>
+                </h-col>
+            </h-row>
+        </h-form-item>
+        
+        <h-form-item>
+            <h-button type="primary" @click="handleSubmit('formValidate')">提交</h-button>
+            <h-button type="ghost" @click="handleReset('formValidate')" style="margin-left: 8px">重置</h-button>
+        </h-form-item>
+    </h-form>
 </div>
 </template>
 <script>
-let jsonData =require('../assets/aa.json');
-export default {
-    data () {
-        return {
-            //  model2:["2018-08-16", "2018-08-23"],
-            model2:"2017-11-13 00:00:00 - 2018-08-23 00:00:00 ",
-            value1: '2016-01-01',
-            model3:"2018-08-16 - 2018-08-24",
-            jsonData:jsonData,
-            columns: [
-              {
-                title: '姓名',
-                key: 'name',
-                width: 100,
-                ellipsis:true,
-                hiddenCol: true,
-              },
-              {
-                title: '姓名1',
-                key: 'name',
-                ellipsis:true,
-              },
-              {
-                title: '年龄',
-                key: 'age',
-                width: 100,
-                ellipsis:true,
-                sortable: true
-              },
-              {
-                title: '年龄1',
-                key: 'age',
-                ellipsis:true,
-                sortable: true
-              },
-              {
-                title: '省份',
-                key: 'province',
-                width: 100,
-                ellipsis:true,
-                sortable: true
-              },{
-                title: '省份1',
-                key: 'province',
-                ellipsis:true
-              },
-              {
-                title: '市区',
-                key: 'city',
-                width: 100,
-                ellipsis:true,
-                sortable: true
-              },{
-                title: '市区1',
-                key: 'city',
-                ellipsis:true
-              },
-              {
-                type: 'text',
-                title: '地址',
-                key: 'address',
-                width: 200,
-                ellipsis:true
-              },{
-                type: 'text',
-                title: '地址1',
-                key: 'address',
-                ellipsis:true
-              },
-              {
-                title: '邮编',
-                key: 'zip',
-                width: 120,
-                ellipsis:true,
-                headerTooltip: true
-              },
-              {
-                title: '邮编1',
-                key: 'zip',
-                ellipsis:true,
-                headerTooltip: true
-              },
-              {
-                title: '操作',
-                key: 'action',
-                render: (h, params) => {
-                  return h('div', [
-                    h('h-button', {
-                      props: {
-                        type: 'info',
-                        size: 'small'
-                      }
-                    }, '查看'),
-                    h('h-button', {
-                      props: {
-                        type: 'text',
-                        size: 'small'
-                      }
-                    }, '编辑')
-                  ]);
+    export default {
+        data () {
+            return {
+                formValidate: {
+                    name: '',
+                    mail: '',
+                    city: '',
+                    gender: '',
+                    interest: [],
+                    date: '',
+                    time: '',
+                    desc: '',
+                    timerange: ''
                 },
-                ellipsis:true
-              },
-              {
-                title: '邮编1',
-                key: 'zip',
-                ellipsis:true,
-                headerTooltip: true,
-                hiddenCol: true,
-              },
-            ],
-            data1: [
-              {
-                name: 0,
-                age: 18,
-                address: '北京市朝阳区\r芍药居',
-                // _disabled:true,
-                _checked:true,
-              },
-              {
-                name: 0,
-                age: 25,
-                address: '北京市海淀区西二旗'
-              },
-              {
-                name: '李小红',
-                age: 30,
-                address: '上海市浦东新区世纪大道'
-              },
-              {
-                name: '周小伟',
-                age: 26,
-                address: '深圳市南山区深南大道'
-              }
-            ],
-
-
+                ruleValidate: {
+                    name: [
+                        { required: true, message: '姓名不能为空', trigger: 'blur' }
+                    ],
+                    mail: [
+                        { required: true, message: '邮箱不能为空', trigger: 'blur' },
+                        { type: 'email', message: '邮箱格式不正确', trigger: 'blur' }
+                    ],
+                    city: [
+                        { required: true, message: '请选择城市', trigger: 'change' }
+                    ],
+                    gender: [
+                        { required: true, message: '请选择性别', trigger: 'change' }
+                    ],
+                    interest: [
+                        { required: true, type: 'array', min: 1, message: '至少选择一个爱好', trigger: 'change' },
+                        { type: 'array', max: 2, message: '最多选择两个爱好', trigger: 'change' }
+                    ],
+                    date: [
+                        { required: true, type: 'date', message: '请选择日期', trigger: 'change' }
+                    ],
+                    time: [
+                        { required: true, type: 'string', message: '请选择时间', trigger: 'change' }
+                    ],
+                    desc: [
+                        { required: true, message: '请输入个人介绍', trigger: 'blur' },
+                        { type: 'string', min: 20, message: '介绍不能少于20字', trigger: 'blur' }
+                    ]
+                }
+            }
+        },
+        methods: {
+            handleSubmit (name) {
+                this.$refs[name].validate((valid) => {
+                    if (valid) {
+                        this.$Message.success('提交成功!');
+                    } else {
+                        this.$Message.error('表单验证失败!');
+                    }
+                })
+            },
+            handleReset (name) {
+                this.$refs[name].resetFields();
+            }
         }
-    },
-    methods: {
-      handleChange1(val) {
-        // console.log('handleChange---->', val)
-      },
-      click1() {
-
-      },
     }
-}
 </script>
