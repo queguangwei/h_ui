3b878bb9e54d40297a153763b50c095b8d99a1a1
AnonaWang <hs_ui_jresUI@163.com>
2019-11-14 14:33:42 +0800

    修改说明：多选时输入框长度需要与query同步增长；删除inputdelete的removeTag(无对应实现)

--- src/components/SelectTree/SelectTree.vue
+++ src/components/SelectTree/SelectTree.vue
@@ -673,7 +673,8 @@ export default{
     },
     handleInputDelete() {
       if (this.multiple && this.model.length && this.query === '') {
-        this.removeTag(this.model.length - 1)
+        // 无匹配方法，故先注释
+        // this.removeTag(this.model.length - 1)
       }
     },
     handleKeydown(e) {
@@ -815,6 +816,8 @@ export default{
     },
     query(val) {
       let query = val || this.lastquery
+      // 输入框的长度需要与query同步增长--多选
+      this.inputLength = val.length * 5 + 20
       //  query改变时触发remote，兼容check选中后（lastquery有值）重复触发
       if (this.remote && this.remoteMethod && (!this.model || this.model.length == 0)) {
         this.remoteMethod(query)
